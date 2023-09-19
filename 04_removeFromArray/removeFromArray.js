31dddf5e124ef8b5c5c648d88b88d4604d2f79d0
Alberto Huang He <aahuanghe@gmail.com>
2023-09-18 23:34:34 -0700

    ah - Finish lesson 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
 const removeFromArray = function() {
-
+    let arr = arguments[0];
+    for(let i = 1; i < arguments.length; i++){
+        for(let j = 0; j < arr.length; j++){
+            if(arguments[i] === arr[j]) {
+                arr.splice(j, 1);
+                break; // doesn't take into account repeats
+            }
+        }
+    }
+    //console.log(arr);
+    return arr;
 };
 
 // Do not edit below this line
