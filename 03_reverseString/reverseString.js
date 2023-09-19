d999a88dbf1486fafbb14d6b4832a7a7895d4fc0
Alberto Huang He <aahuanghe@gmail.com>
2023-09-18 22:02:04 -0700

    ah - Finish exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let temp = '';
+    if(str === '') return temp;
+    for(let i = (str.length - 1); i >= 0; i--){
+        temp += str[i];
+    }
+    return temp;
 };
 
 // Do not edit below this line
