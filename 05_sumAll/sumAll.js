a46472bc8b4567e7d9b94294f0a4550f7d91be97
Alberto Huang He <aahuanghe@gmail.com>
2023-09-19 00:01:24 -0700

    ah - Finish lesson 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if(num1 < 0 || num2 < 0) return 'ERROR';
+    if(typeof(num1) !== 'number' || typeof(num2) !== 'number') return 'ERROR';
 
+    let sum = 0;
+    const low = (num1 < num2) ? num1 : num2;
+    const high = (num1 >= num2) ? num1 : num2;
+
+    for(let i = low; i <= high; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
